076fc9bc1891ddc3c8888ca87657b235a81ca554
Hodge-zf <xx.skcs@gmail.com>
2020-10-22 21:41:13 -0500

    Task 1 complete

--- Assignment2/shellCall_ls.c
+++ Assignment2/shellCall_ls.c
@@ -3,11 +3,14 @@
 #include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 /* Simple shell program that run ls command
  - Create a child process
  - Parent wait child process to terminate
  - calls the ls command */
 int main() {
+
+  char command[25];
 
   char *username;
   username = (char *)malloc(10*sizeof(char));
@@ -16,7 +19,13 @@ int main() {
   time_t now;
   time(&now);
 
-  printf("<%s, %s >",username,ctime(&now));
+  printf("<%s, %s>\n",username,ctime(&now));
+  printf("Please type a Unix command:\n");
+
+  scanf("%s", command);
+  char directory[35] = "/usr/bin/";
+
+  strcat(directory,command);
 
       pid_t  pid;
 	/* fork another process */
@@ -26,11 +35,11 @@ int main() {
 		exit(-1);
 	}
 	else if (pid == 0) { /* child process */
-		execlp("/bin/ls", "ls", NULL);
+		execlp(directory, command, NULL);
 	}
 	else { /* parent process */
 		/* parent will wait for the child to complete */
-		wait (NULL);
-		printf ("Child Complete");
+		wait(NULL);
+		printf("Child Complete");
 		exit(0); 	       }
    }
